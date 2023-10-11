d7798d9d22ba174653a8e1fdf002ba9f5411a5ef
haakoek <h.e.kristiansen@kjemi.uio.no>
2023-10-11 11:56:45 +0200

    Add mapping functions from Legendre-Lobatto points to radial grid points.

--- grid_methods/spherical_coordinates/gauss_legendre_lobatto.py
+++ grid_methods/spherical_coordinates/gauss_legendre_lobatto.py
@@ -3,80 +3,89 @@ from numpy.polynomial import legendre
 from scipy.special import eval_legendre as Legendre
 
 
+class Rational_map:
+    def __init__(self, r_max=30, alpha=0.4):
+
+        self.r_max = r_max
+        self.alpha = alpha
+        self.L_scale = self.r_max * self.alpha / 2
+
+    def r_x(self, x):
+        return self.L_scale * (1 + x) / (1 - x + self.alpha)
+
+    def dr_dx(self, x):
+        return (2 + self.alpha) * self.L_scale / (1 - x + self.alpha) ** 2
+
+
+class Linear_map:
+    def __init__(self, r_max=30):
+        self.r_max = r_max
+
+    def r_x(self, x):
+        return self.r_max * (x + 1) / 2
+
+    def dr_dx(self, x):
+        return self.r_max / 2 * np.ones(x.shape)
+
+
 class GaussLegendreLobatto:
-    def __init__(self, x0, xN, N_tot):
-        self.x0 = x0
-        self.xN = xN
-        self.N_tot = N_tot
-        self.set_grid(self.x0, self.xN, self.N_tot)
-
-    def quad_bp(self, y):
-        return np.sum(self.qw * y)
-
-    def quad(self, y):
-        """Gaussian quadrature of y = y(x), a function evaluated on the grid."""
-        return np.sum(self.qw[1:-1] * y)
-
-    def set_grid(self, x0, xN, N_tot):
-        """
-        Compute nodes, weights and differentiation matrix for Gauss-Legendre-Lobatto
-        quadrature on the interval [a, b].
-
-        Parameters
-        ----------
-        a :
-            left endpoint of the interval
-        b :
-            right endpoint of the interval
-        N_tot :
-            The total number of nodes/grid points including the endpoints.
-
-        Notes
-        -----
-
-
-        """
-
-        # Grid parameters
-        assert N_tot > 0
-
-        ##########################################################
-        N = N_tot - 1
+    def __init__(self, N, Mapping):
 
         # Get inner nodes
         c = np.zeros((N + 1,))
         c[-1] = 1
         dc = legendre.legder(c)
 
-        self.nodes = np.zeros(N + 1)
-        self.nodes[0] = -1
-        self.nodes[-1] = 1
-        self.nodes[1:-1] = legendre.legroots(dc)
+        """
+        Find the Gauss-Legendre-Lobatto grid points, defined as the roots of the derivative N-th order Legendre polynomial,
+            dP_N(x)/dx|_{x=x_i} = 0, i=1,...,N, 
+        and x_0 = -1, x_{N+1} = 1.
+        """
+        self.x = np.zeros(N + 1)
+        self.x[0] = -1
+        self.x[-1] = 1
+        self.x[1:-1] = legendre.legroots(dc)
 
-        # Check for correct number of roots, and uniqueness
-        assert len(self.nodes[1:-1]) == N - 1
-        assert np.allclose(np.unique(self.nodes[1:-1]), self.nodes[1:-1])
+        self.D1 = np.zeros((N + 1, N + 1))
+        self.D2 = np.zeros((N + 1, N + 1))
+        self.PN_x = legendre.legval(self.x[1:-1], c)
 
-        # Compute PN(x_i) and the weights
-        self.PN_x = legendre.legval(self.nodes, c)
-        self.weights = 2 / (N * (N + 1) * self.PN_x**2)
-        ############################################################
+        self.weights = 2 / (N * (N + 1))
 
-        self.Dx = np.zeros((N_tot, N_tot))
-        for i in range(N_tot):
-            for j in range(N_tot):
+        self.r = Mapping.r_x(self.x)
+        self.r_dot = Mapping.dr_dx(self.x)
+
+        for i in range(N + 1):
+            for j in range(N + 1):
                 if i == 0 and j == 0:
-                    self.Dx[i, j] = 1 / 4 * (N_tot - 1) * N_tot
-                elif i == N_tot - 1 and j == N_tot - 1:
-                    self.Dx[i, j] = -1 / 4 * (N_tot - 1) * N_tot
-                elif (i == j) and (0 < j < N_tot - 1):
-                    self.Dx[i, j] = 0
+                    self.D1[i, j] = (
+                        0.25 * N * (N + 1) / np.sqrt(self.r_dot[i] * self.r_dot[j])
+                    )
+                elif i == N and j == N:
+                    self.D1[i, j] = (
+                        -0.25 * N * (N + 1) / np.sqrt(self.r_dot[i] * self.r_dot[j])
+                    )
+                elif i == j and 1 <= j <= N - 1:
+                    self.D1[i, j] = 0
                 else:
-                    self.Dx[i, j] = self.PN_x[i] / (
-                        self.PN_x[j] * (self.nodes[i] - self.nodes[j])
+                    self.D1[i, j] = 1 / (
+                        (self.x[i] - self.x[j]) * np.sqrt(self.r_dot[i] * self.r_dot[j])
                     )
 
-        # scale domain and diff matrix
-        self.nodes = (xN - x0) * (self.nodes + 1) / 2 + x0
-        self.Dx = 2 / (xN - x0) * self.Dx
-        self.qw = self.weights * (xN - x0) / 2
+        for i in range(1, N - 1):
+            for j in range(1, N - 1):
+                if i == j:
+                    self.D2[i, j] = (
+                        -1
+                        / 3
+                        * N
+                        * (N + 1)
+                        / (1 - self.x[i] ** 2)
+                        / (self.r_dot[i] * self.r_dot[j])
+                    )
+                else:
+                    self.D2[i, j] = (
+                        -2
+                        / (self.x[i] - self.x[j]) ** 2
+                        / (self.r_dot[i] * self.r_dot[j])
+                    )
