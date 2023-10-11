6cd2801082d30bb6cc7121b1d6d83f4d75218ecb
haakoek <h.e.kristiansen@kjemi.uio.no>
2023-10-11 14:49:59 +0200

    Update init files.

--- grid_methods/__init__.py
+++ grid_methods/__init__.py
@@ -1,1 +1,8 @@
-from .spherical_coordinates import GaussLegendreLobatto, Rational_map, Linear_map
+from .spherical_coordinates import (
+    GaussLegendreLobatto,
+    Rational_map,
+    Linear_map,
+    Coulomb,
+    SAE,
+    Gaussian_charge_distribution,
+)
--- grid_methods/spherical_coordinates/__init__.py
+++ grid_methods/spherical_coordinates/__init__.py
@@ -3,3 +3,5 @@ from .gauss_legendre_lobatto import (
     Rational_map,
     Linear_map,
 )
+
+from .potentials import Coulomb, SAE, Gaussian_charge_distribution
