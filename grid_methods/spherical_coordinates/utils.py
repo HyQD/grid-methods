a54a5d518bc16745816d70d6d6670c7f5a79de54
Tui <eanorway@gmail.com>
2023-10-13 16:23:20 +0200

    vectorized mask function evaluation

--- grid_methods/spherical_coordinates/utils.py
+++ grid_methods/spherical_coordinates/utils.py
@@ -94,11 +94,18 @@ def compute_numerical_states(l_max, n_max, r):
     return eigenenergies, eigenstates
 
 
-def mask_function(r, r_max, r0):
-    if r < r0:
-        return 1
-    else:
-        return np.cos(np.pi * (r - r0) / (2 * (r_max - r0))) ** (1 / 8)
+def mask_function(r, r_max, r0, n=4):
+    mask_r = np.zeros(len(r))
+
+    ind1 = r < r0
+    ind2 = r == r_max
+    ind3 = np.invert(ind1 + ind2)
+
+    mask_r[ind1] = 1
+    mask_r[ind2] = 0
+    mask_r[ind3] = np.cos(np.pi * (r[ind3] - r0) / (2 * (r_max - r0))) ** (1 / n)
+
+    return mask_r
 
 
 @jit(nopython=True)
@@ -228,3 +235,16 @@ def compute_hhg_spectrum(time_points, dipole_moment, hann_window=False):
     )
 
     return omega, Px
+
+
+class Counter:
+    # Used to count iterations until convergence in bicgstab
+    def __init__(self):
+        self.counter = 0
+
+    def __call__(self, x):
+        self.counter += 1
+
+
+def quadrature(weights, f):
+    return np.sum(weights * f)
