29463b96248ffdb4a699ce4fa399e1658d43fd5f
haakoek <h.e.kristiansen@kjemi.uio.no>
2023-11-01 16:54:02 +0100

    Add ionization probability function.

--- grid_methods/spherical_coordinates/properties.py
+++ grid_methods/spherical_coordinates/properties.py
@@ -1,3 +1,4 @@
+import numpy as np
 from opt_einsum import contract
 
 from grid_methods.spherical_coordinates.utils import quadrature
@@ -30,3 +31,28 @@ def expec_p_i(psi, dpsi_dr, weights, r, xi_Omega, H_xi_beta):
         expec_pi -= 1j * quadrature(weights, (psi[I].conj() / r) * tmp_beta[I])
 
     return expec_pi
+
+
+def norm(psi, weights):
+    n_lm = psi.shape[0]
+    norm = 0 + 0j
+    for I in range(n_lm):
+        norm += quadrature(weights, np.abs(psi[I]) ** 2)
+    return norm
+
+
+def compute_ionization_probability(psi, r, radius=20):
+    """
+    Here the (single) ionization probaility is taken as the probability of finding an electron outside a sphere
+    of a given radius.
+
+    P_{ionization}(t) = 1-\int_0^{radius} |\Psi(\mathfbf{r},t)|^2 r^2 dr d\Omega
+    """
+    n_lm = psi.shape[0]
+    r_rad = r[r <= radius]
+    ionization_prob = 0
+    for I in range(n_lm):
+        psi_rad = psi[I][r <= radius]
+        ionization_prob += quadrature(weights, np.abs(psi_rad) ** 2)
+
+    return 1 - ionization_prob
