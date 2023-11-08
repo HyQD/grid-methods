55618cd8dacd834c0b368ada7b8c86fd92099dff
haakoek <h.e.kristiansen@kjemi.uio.no>
2023-11-08 17:28:12 +0100

    Add class for Hamiltonian with static B-field along the z-axis.

--- grid_methods/spherical_coordinates/rhs.py
+++ grid_methods/spherical_coordinates/rhs.py
@@ -11,16 +11,16 @@ class H0Psi:
     ):
         self.n_lm = angular_matrix_elements.n_lm
         self.nr = radial_matrix_elements.nr
-        lm_I = angular_matrix_elements.lm_I
-        r = radial_matrix_elements.r
+        self.lm_I = angular_matrix_elements.lm_I
+        self.r = radial_matrix_elements.r
 
         self.potential = potential
         self.T_D2 = -(1 / 2) * radial_matrix_elements.D2
 
-        self.centrifugal_force_r = 1 / (2 * r**2)
+        self.centrifugal_force_r = 1 / (2 * self.r**2)
         self.centrifugal_force_lm = np.zeros(self.n_lm)
         for I in range(self.n_lm):
-            l, m = lm_I[I]
+            l, m = self.lm_I[I]
             self.centrifugal_force_lm[I] = l * (l + 1)
 
     def __call__(self, psi, t, ravel=True):
@@ -37,8 +37,39 @@ class H0Psi:
             return psi_new
 
 
+class H0_B_Psi(H0Psi):
+    def __init__(
+        self, angular_matrix_elements, radial_matrix_elements, potential, B0
+    ):
+        super().__init__(
+            angular_matrix_elements,
+            radial_matrix_elements,
+            potential,
+        )
+        self.B0 = B0
+
+        self.H_Bz_Omega = angular_matrix_elements("H_Bz_Omega")
+
+    def __call__(self, psi, t, ravel=True):
+        psi_new = super().__call__(psi, t, ravel=False)
+        tmp_B = contract("IJ, Jk->Ik", self.H_Bz_Omega, psi)
+        psi_new += contract(
+            "Ik, k->Ik", tmp_B, (self.B0**2 / 8) * self.r**2
+        )
+        for I in range(self.n_lm):
+            l, m = self.lm_I[I]
+            psi_new[I] += self.B0 * m / 2 * psi[I]
+
+        if ravel:
+            return psi_new.ravel()
+        else:
+            return psi_new
+
+
 class VPsi:
-    def __init__(self, angular_matrix_elements, radial_matrix_elements, **kwargs):
+    def __init__(
+        self, angular_matrix_elements, radial_matrix_elements, **kwargs
+    ):
         self.angular_matrix_elements = angular_matrix_elements
         self.radial_matrix_elements = radial_matrix_elements
         self.__dict__.update(kwargs)
@@ -53,7 +84,11 @@ class VPsi:
 
 class HtPsi:
     def __init__(
-        self, angular_matrix_elements, radial_matrix_elements, H0_psi, V_psi_list
+        self,
+        angular_matrix_elements,
+        radial_matrix_elements,
+        H0_psi,
+        V_psi_list,
     ):
         self.H0_psi = H0_psi
         self.V_psi_list = V_psi_list
