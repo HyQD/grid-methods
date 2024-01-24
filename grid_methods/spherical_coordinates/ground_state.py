7dbf69084e2182a2a8d3aa33dd434e1709264754
Tui <eanorway@gmail.com>
2024-01-24 16:39:41 +0100

    ground state calculation with extra Coulomb potential

--- grid_methods/spherical_coordinates/ground_state.py
+++ grid_methods/spherical_coordinates/ground_state.py
@@ -17,3 +17,31 @@ def compute_ground_state(angular_matrix_elements, radial_matrix_elements, potent
     eps, phi_n = np.linalg.eigh(H0)
 
     return eps, phi_n
+
+
+def compute_ground_state_diatomic(
+    angular_matrix_elements, radial_matrix_elements, potential, l_max
+):
+    nl = l_max + 1
+    nr = radial_matrix_elements.nr
+    r = radial_matrix_elements.r
+    T_D2 = -(1 / 2) * radial_matrix_elements.D2
+
+    clmb_ = angular_matrix_elements("1/(r-a)")
+    clmb = np.zeros((nl, nl, nr, nr), dtype=np.complex128)
+    clmb[:, :, np.arange(nr), np.arange(nr)] = clmb_
+    clmb = clmb.swapaxes(1, 2).swapaxes(0, 1).swapaxes(2, 3).T
+    clmb = clmb.reshape(nr * nl, nr * nl)
+
+    H0 = np.zeros((nr * nl, nr * nl))
+    for l in range(nl):
+        T = T_D2 + np.diag(l * (l + 1) / (2 * r**2))
+        V = np.diag(potential)
+
+        H0[l * nr : ((l + 1) * nr), l * nr : ((l + 1) * nr)] = T + V
+    assert np.allclose(H0, H0.T)
+
+    H0 = H0 - clmb
+    eps, phi_n = np.linalg.eigh(H0)
+
+    return eps, phi_n
