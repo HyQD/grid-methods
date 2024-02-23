c78be09e4f75689d8e81896da562a66dd9fe8985
Tui <eanorway@gmail.com>
2024-02-24 00:09:24 +0100

    made l a parameter in ground state function

--- grid_methods/spherical_coordinates/ground_state.py
+++ grid_methods/spherical_coordinates/ground_state.py
@@ -2,15 +2,15 @@ import numpy as np
 
 
 def compute_ground_state(
-    angular_matrix_elements, radial_matrix_elements, potential, hermitian=True
+    angular_matrix_elements, radial_matrix_elements, potential, l=0, hermitian=True
 ):
     nr = radial_matrix_elements.nr
     r = radial_matrix_elements.r
     T_D2 = -(1 / 2) * radial_matrix_elements.D2
 
     H0 = np.zeros((nr, nr))
-    l_init = 0
-    T = T_D2 + np.diag(l_init * (l_init + 1) / (2 * r**2))
+    l = 0
+    T = T_D2 + np.diag(l * (l + 1) / (2 * r**2))
     V = np.diag(potential)
 
     H0 = T + V
