d5fd808ca7811ecfc08af5f44c230de8c30bc13d
haakoek <h.e.kristiansen@kjemi.uio.no>
2024-05-16 11:55:31 +0200

    Set center of Coulomb potential.

--- grid_methods/cartesian_coordinates/potentials.py
+++ grid_methods/cartesian_coordinates/potentials.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-def Coulomb(x, Z, a=1.0):
-    return -Z / np.sqrt(x**2 + a**2)
+def Coulomb(x, Z, x_c=0.0, a=1.0):
+    return -Z / np.sqrt((x - x_c) ** 2 + a**2)
 
 
 def harmonic_oscillator(x, omega):
