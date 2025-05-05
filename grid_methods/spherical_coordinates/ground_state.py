270dfae5487e6b60e4ba0cf479054ae8d364b861
haakoek <h.e.kristiansen@kjemi.uio.no>
2025-05-05 14:36:16 +0200

    Add convenience function for getting normalized hydrogenic orbitals.

--- grid_methods/spherical_coordinates/ground_state.py
+++ grid_methods/spherical_coordinates/ground_state.py
@@ -1,8 +1,78 @@
 import numpy as np
 
 
+def get_hydrogenic_orbitals(GLL, l_max, n_s, Z):
+
+    """
+    Compute the n_s lowest lying normalized hydrogenic orbitals for l=0,...,l_max.
+
+    The orbitals are normalized such that
+    .. math::
+        \int_0^{r_{max}} |u_{n,l}(r)|^2 dr = 1 (1)
+    where :math:`u_{n,l}(r)` is the radial part of the n-th eigenfunction
+    .. math::
+        \psi_{n,l,m}(\mathbf{r}) = r^{-1}u_{n,l}(r) Y_{lm}(\theta, \phi),
+    of the hydrogenic Hamiltonian
+    .. math::
+        H = -\frac{1}{2} \nabla^2 - \frac{Z}{r}.
+
+    The integral given by Eq.(1) is computed using the Gauss-Legendre-Lobatto quadrature rule.
+
+    Parameters
+    ----------
+    GLL : GaussLegendreLobatto
+        Gauss-Legendre-Lobatto grid object.
+    l_max : int
+        Maximum angular momentum quantum number.
+    n_s : list
+        List of number of eigenstates for each l.
+    Z : float
+        Nuclear charge.
+    Returns
+    -------
+    orbitals : dict
+        Dictionary of orbitals for each l.
+    orbital_energy : dict
+        Dictionary of orbital energies for each l.
+    """
+
+    r = GLL.r[1:-1]
+    w_r = GLL.weights[1:-1]
+    r_dot = GLL.r_dot[1:-1]
+    D1 = GLL.D1
+    D2 = np.dot(D1, D1)[1:-1, 1:-1]
+
+    orbitals = dict()
+    orbital_energy = dict()
+
+    for l in range(0, l_max + 1):
+
+        Tl = -0.5 * D2 + np.diag(l * (l + 1) / (2 * r**2))
+        V = np.diag(-Z / r)
+        Hl = Tl + V
+        eps, U = np.linalg.eig(Hl)
+
+        idx = np.argsort(eps)
+        eps = eps[idx]
+        U = U[:, idx]
+
+        n_states = n_s[l]
+        B = U[:, 0:n_states]
+        for i in range(n_states):
+            norm_psi = np.dot(w_r, r_dot * B[:, i] * B[:, i])
+            B[:, i] /= np.sqrt(norm_psi)
+        orbitals[f"{l}"] = B.copy()
+        orbital_energy[f"{l}"] = eps[0:n_states].copy()
+
+    return orbitals, orbital_energy
+
+
 def compute_ground_state(
-    angular_matrix_elements, radial_matrix_elements, potential, l=0, hermitian=True
+    angular_matrix_elements,
+    radial_matrix_elements,
+    potential,
+    l=0,
+    hermitian=True,
 ):
     nr = radial_matrix_elements.nr
     r = radial_matrix_elements.r
@@ -24,7 +94,11 @@ def compute_ground_state(
 
 
 def compute_ground_state_diatomic(
-    angular_matrix_elements, radial_matrix_elements, potential, l_max, hermitian=True
+    angular_matrix_elements,
+    radial_matrix_elements,
+    potential,
+    l_max,
+    hermitian=True,
 ):
     nl = l_max + 1
     nr = radial_matrix_elements.nr
