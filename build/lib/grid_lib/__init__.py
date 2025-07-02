1155eb40773fc8aa8fbabdb84518cc12f9fda8f2
haakoek <h.e.kristiansen@kjemi.uio.no>
2025-07-02 17:05:36 +0200

    Remove build folder.

--- build/lib/grid_lib/__init__.py
+++
@@ -1,6 +0,0 @@
-from .version import __version__
-from .spherical_coordinates import (
-    Coulomb,
-    SAE,
-    Gaussian_charge_distribution,
-)
--- build/lib/grid_lib/spherical_coordinates/__init__.py
+++
@@ -1,2 +0,0 @@
-from .potentials import Coulomb, SAE, Gaussian_charge_distribution
-from .propagators import Propagator, BiCGstab
